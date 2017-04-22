521bde5e3bc0ee1cfdb6fe8a89bbaa099aca609b
rhinel <rhinelkc@hotmail.com>
2017-04-22 21:07:29 +0800

    finall

--- myhomepage.sh
+++ myhomepage.sh
@@ -5,9 +5,7 @@ docker rm -f myhomepage
 # 启动并自动重启
 docker run \
  --name myhomepage \
- -p 3080:80 \
- -p 3443:443 \
+ -expose 80 \
  -v $PWD:/webapp \
- -v $PWD/../ssl-key:/ssl-key \
  -w /webapp \
  -d node npm start
