09d60575e592e306c0bc76c610adcb6c377c5960
rhinel <rhinelkc@hotmail.com>
2017-05-10 23:54:34 +0800

    v0 lean

--- server/index.js
+++ server/index.js
@@ -5,18 +5,15 @@ let http = require('http')
 let fs = require('fs')
 let path = require('path')
 
-let express = require('express')
-let bodyParser = require('body-parser')
-
 //启动路由及端口处理
 let app = express()
 //http转发
 let httpServer = http.createServer(app)
 const httpPORT = process.env.HTTPPORT || 80
 app.use('/', express.static(path.resolve(__dirname, '../')))
-//使用post&json
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.get('*', (req, res)=>{
+	res.send(fs.readFileSync(path.resolve('../404/404.html'), 'utf-8'))
+})
 
 //启动监听
 console.log('--------------------------------------')
